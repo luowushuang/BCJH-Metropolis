0ccdf726143328f49a9de2a3eeb01a4903b05e44
luowushuang <134758214+luowushuang@users.noreply.github.com>
2023-06-28 23:49:15 +0800

    toolEquipped.hpp

--- toolEquipped.hpp
+++ toolEquipped.hpp
@@ -24,6 +24,8 @@
 
 void toolEquipped(Chef *chef) {
 
+    //return; // 以下为示例代码，可自行修改。完成后注释此行。
+    
     auto skill = &chef->skill; // 这句不用管，放着就好。
 
     if (chef->name == "达浪") {              // 智能小鱼板
@@ -40,7 +42,5 @@ void toolEquipped(Chef *chef) {
         skill->abilityBuff.boil += 30; // 煮售价+30%
         chef->NoTool();                // 不允许装备其他厨具
     }
-    
-    return; // 以下为示例代码，可自行修改。完成后注释此行。    
 }
 #endif
